e7c70a28bd640e16247dde9a2678d020e9bd4357
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2025-02-12 20:53:04 +0200

    Added exercises after the lecture

---
+++ ConditionalStatementsAdvancedExercise/P06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace P06.OperationsBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            char operatorSymbol = char.Parse(Console.ReadLine());
+
+            double result = 0;
+            string evenOrOdd = "";
+
+            if (operatorSymbol == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (operatorSymbol == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (operatorSymbol == '*')
+            {
+                result = num1 * num2;
+            }
+
+            if (num2 == 0 || num1 == 0)
+            {
+                Console.WriteLine($"Cannot divide {num1} by zero");
+            }
+            else
+            {
+                if (operatorSymbol == '/')
+                {
+                    result = (double)num1 / num2;
+                    Console.WriteLine($"{num1} {operatorSymbol} {num2} = {result}");
+                }
+                else if (operatorSymbol == '%')
+                {
+                    result = num1 % num2;
+                    Console.WriteLine($"{num1} {operatorSymbol} {num2} = {result}");
+                }
+            }
+
+            if (operatorSymbol == '+' ||
+                operatorSymbol == '-' ||
+                operatorSymbol == '*')
+            {
+                if (result % 2 == 0)
+                {
+                    evenOrOdd = "even";
+                }
+                else
+                {
+                    evenOrOdd = "odd";
+                }
+                Console.WriteLine($"{num1} {operatorSymbol} {num2} = {result} - {evenOrOdd}");
+            }
+            
+        }
+    }
+}
---
+++ ConditionalStatementsAdvancedExercise/P07.HotelRoom/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace P07.HotelRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nightsCount = int.Parse(Console.ReadLine());
+
+            decimal studioTotalPrice = 0;
+            decimal apartmentTotalPrice = 0;
+
+            decimal discountPercent = 0;
+            if (month == "May" || month == "October")
+            {
+                studioTotalPrice = nightsCount * 50;
+                apartmentTotalPrice = nightsCount * 65;
+                if (nightsCount > 7 && nightsCount <= 14)
+                {
+                    discountPercent = 0.05m;
+                                                //   250лв - 12.5 = 237.5
+                    studioTotalPrice = studioTotalPrice - studioTotalPrice * discountPercent;
+                }
+                else if (nightsCount > 14)
+                {
+                    discountPercent = 0.3m;
+
+                    studioTotalPrice = studioTotalPrice - studioTotalPrice * discountPercent;
+                }
+
+            }
+            else if (month == "June" || month == "September")
+            {
+                studioTotalPrice = nightsCount * 75.20m;
+                apartmentTotalPrice = nightsCount * 68.70m;
+                if (nightsCount > 14)
+                {
+                    discountPercent = 0.2m;
+                    studioTotalPrice = studioTotalPrice - studioTotalPrice * discountPercent;
+                }
+            }
+            else if (month == "July" || month == "August")
+            {
+                studioTotalPrice = nightsCount * 76;
+                apartmentTotalPrice = nightsCount * 77;
+            }
+
+            if (nightsCount > 14)
+            {
+                discountPercent = 0.1m;
+
+                apartmentTotalPrice = apartmentTotalPrice - apartmentTotalPrice * discountPercent;
+            }
+
+            Console.WriteLine($"Apartment: {apartmentTotalPrice:f2} lv.");
+            Console.WriteLine($"Studio: {studioTotalPrice:f2} lv");
+        }
+    }
+}
