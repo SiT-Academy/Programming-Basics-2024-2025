2382b45e31550b03db8f1afd68cc183c97322d3f
JacksonJS12 <d.ivan0v.6100@gmail.com>
2025-04-16 20:34:55 +0300

    After lecture exersices.

---
+++ ForLoopExercise/P05.Salary/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double salaray = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string site = Console.ReadLine();
+                switch (site)
+                {
+                    case "Facebook":
+                        salaray -= 150;
+                        break;
+                    case "Instagram":
+                        salaray -= 100;
+                        break;
+                    case "Reddit":
+                        salaray -= 50;
+                        break;
+                }
+
+                if (salaray <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (salaray > 0)
+            {
+                Console.WriteLine(salaray);
+            }
+        }
+    }
+}
